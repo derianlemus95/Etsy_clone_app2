a7a1f650b96664c32a8044ce8eda9de39be21a45
derianlem <42731136+derianlem@users.noreply.github.com>
2022-04-26 22:43:13 -0700

    Update config

--- Backend/Utils/config.js
+++ Backend/Utils/config.js
@@ -2,7 +2,7 @@ const config = {
   secret: "cmpe273_secret_key",
   frontendURL: "http://localhost:3000",
   mongoDB:
-    "mongodb+srv://newuser:password12345@cluster0.iawvu.mongodb.net/etsyclone2",
+    "http:mongourlgoeshere",
 };
 
 module.exports = config;
